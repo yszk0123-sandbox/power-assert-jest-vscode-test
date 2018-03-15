58ef087ef2510a71f8d97b9c46a72d5705440d60
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-03-15 17:04:13 +0900

    Eject

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import assert from 'assert';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  assert({ a: 1 } === { a: 3 });
 });
